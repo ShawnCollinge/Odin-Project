1f47aa3fec69d9bd4f3b2a1a36acbf228e8fe85e
Shawn Collinge <shawncollinge@gmail.com>
2022-09-03 19:51:26 +0000

    Finish updated rock-paper-scissors game

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,6 +1,8 @@
-let choices = ["Rock", "Paper", "Scissors"];
+var playerScore = 0;
+var computerScore = 0;
 
 function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
 }
@@ -8,24 +10,30 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
+            computerScore++;
             return "You Lose! Paper beats rock";
         } else if (computerSelection === "Scissors") {
+            playerScore++;
             return "You win! Rock beats Scissors";
         } else {
             return "Draw! You both chose Rock";
         }
     } else if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
+            playerScore++;
             return "You win! Paper beats Rock";
         } else if (computerSelection === "Scissors") {
+            computerScore++;
             return "You lose! Scissors beat Paper";
         } else {
             return "Draw! You both chose Paper";
         }
     } else {
         if (computerSelection === "Paper") {
+            playerScore++;
             return "You win! Scissors beats Paper";
         } else if (computerSelection === "Rock") {
+            computerScore++;
             return "You lose! Rock beats scissors";
         } else {
             return "Draw, You both chose scissors";
@@ -33,22 +41,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function getPlayerChoice() {
-    let playerSelection = prompt("Please chose Rock, Paper, or scissors");
-    playerSelection = playerSelection.toLowerCase();
-    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = getPlayerChoice();
-
-        if (choices.includes(playerSelection)) {
-            console.log(playRound(playerSelection, getComputerChoice()));
-        } else {
-            console.log("Invalid choice");
-        }
+function updateScore() {
+    if (playerScore > 4) {
+        score.innerText = `You win! You beat the computer ${playerScore} - ${computerScore}`
+    } else if (computerScore > 4) {
+        score.innerText = `You lost! You lost to the computer ${computerScore} - ${playerScore}`
+    } else {
+        score.innerText = `Player score: ${playerScore} Computer score: ${computerScore}`;
     }
 }
 
-game();
+const buttons = document.querySelectorAll("button");
+let para = document.querySelector("p");
+let score = document.querySelector("div");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (playerScore < 5 && computerScore < 5) {
+            para.innerText = playRound(button.id, getComputerChoice());
+            updateScore();
+        }
+    });
+});

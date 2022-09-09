abcd36a3466defc64632606d0d7efb6f79e03cc3
Shawn Collinge <shawncollinge@gmail.com>
2022-09-09 21:05:55 +0000

    javascript practice

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (...values) {
+  let result = 0;
+  values.forEach((value) => result += value);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = (a, b) => a - b;
+
+const sum = function (numbers) {
+  let result = 0;
+  numbers.forEach(value => result += value);
+  return result;
 };
 
-const sum = function() {
-	
+const multiply = function (values) {
+  if (values.length === 0) {
+    return 0;
+  } else {
+    let result = 1;
+    values.forEach(value => result *= value);
+    return result;
+  }
 };
 
-const multiply = function() {
-
+const power = function (a, b) {
+    let result = a;
+    for (let i = 1; i < b; i++) {
+      result *= a;
+    }
+    return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (a) {
+  let result = 1;
+  for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
